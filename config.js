55d171f5b0d18dc33ff7bba0401aa90c979ecf6d
Lim Chee Aun <cheeaun@gmail.com>
2018-03-04 20:39:37 +0800

    Update to new cloudant instance hosted on IBM cloud

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
 	screen_name: 'cheeaun', // Twitter username
 	github_project_url: 'https://github.com/cheeaun/tweets', // URL to GitHub repo page
-	couchdb_db_url: 'https://cheeaun.cloudant.com/tweets', // URL to 'tweets' DB
+	couchdb_db_url: 'https://dfa8d2c9-a48f-4476-89af-36b841292a10-bluemix.cloudant.com/tweets', // URL to 'tweets' DB
 	couchdb_search: true // Lucene + CouchDB ?
 };
